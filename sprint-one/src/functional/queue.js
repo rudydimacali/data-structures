9b4d14937fe65da8ce7e242aa85cef5680f572b4
Rudy <rudydimacali@gmail.com>
2018-11-07 12:16:16 -0800

    Completed Part 1 Functional Queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    storage[keys.length] = value;
   };
 
   someInstance.dequeue = function() {
+    var newStorage = Object.assign({}, storage);
+    var keys = Object.keys(newStorage);
+    var deleted = storage[0];
+    delete storage[0];
+    for (var i = 0; i < keys.length; i++) {
+      storage[i] = newStorage[i+1];
+    }
+    delete storage[keys.length-1];
+    return deleted;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
