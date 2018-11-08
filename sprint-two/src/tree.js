42d266f7f90a0461c5df168ea8a2451432b9f1db
Hack Reactor Student <student@hackreactor.com>
2018-11-08 11:11:28 -0800

    Completed tree.js.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,40 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  // CHILDREN EXIST
+  if (this.children.length) {
+    var results = _.map(this.children, function(child) {
+      if(child.contains(target)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return results.includes(true);
+  }
+  // NO CHILDREN EXIST
+  return false;
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// .contains would be a linear complexity function. O(n)
+// .addChild would be a constant complexity function. O(1)
