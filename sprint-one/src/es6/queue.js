5244ba84f75926f02331631ba5bfebc5f8582153
Rudy <rudydimacali@gmail.com>
2018-11-10 16:01:58 -0800

    Rewrote es6 queue.js - did not commit/push original.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
   }
+  enqueue(value) {
+	var keys = Object.keys(this.storage);
+	this.storage[keys.length] = value;
+  };
+
+  dequeue() {
+		var newStorage = Object.assign({}, this.storage);
+		var keys = Object.keys(newStorage);
+		var deleted = this.storage[0];
+		delete this.storage[0];
+		for (var i = 0; i < keys.length; i++) {
+		    this.storage[i] = newStorage[i+1];
+		  }
+		  delete this.storage[keys.length-1];
+		  return deleted;
+	};
+
+	size() {
+	  return Object.keys(this.storage).length;
+	};
 
 }
+
